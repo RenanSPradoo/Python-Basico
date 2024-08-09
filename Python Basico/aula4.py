748d7fb0b8d57538fb0036af67ea88d435261742
RenanSPradoo <renansilvaprado2@hotmail.com>
2024-08-09 13:52:51 -0300

    Dissecando uma viaral em PY

---
+++ Python Basico/aula4.py
@@ -0,0 +1,8 @@
+a = input('Digite algo: ')
+print('O tipo premitivo desse valor é ', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É um numero? ', a.isnumeric())
+print('É alfabético? ', a.isalpha())
+print('É alfanumérico? ', a.isalnum())
+print('Está em maiúsculas? ', a.islower())
+print('Está capitalizada? ', a.istitle())
