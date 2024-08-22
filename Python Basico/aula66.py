9551019e8e7a1d170dec7daa8273c0ad5b680b76
RenanSPradoo <renansilvaprado2@hotmail.com>
2024-08-22 00:15:05 -0300

    Usando While

---
+++ Python Basico/aula66.py
@@ -0,0 +1,21 @@
+print('-=' * 10)
+print('Gerador de PA')
+print('-=' * 10)
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razao da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print(f' {termo} ', end=' ➜ ')
+        termo += razao
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais: '))
+print(f'\nPrograssão finalizada com {total} termos mostrados.')
+print()
